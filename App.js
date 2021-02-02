c6c0c3e64dfdc18780ffc0c6904f02cc2506d5ff
NoahCardoza <noahcardoza@gmail.com>
2021-02-02 11:46:23 -0800

    start list views: batches -> days

--- App.js
+++ App.js
@@ -1,28 +1,72 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
-// import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { StyleSheet, SafeAreaView, View } from 'react-native';
+
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 import BatchList from './components/BatchList';
+import DayList from './components/DayList';
 
 const batches = [
   {
     id: 1,
     title: 'Cabernet Sauvignon',
+    days: [
+      {
+        id: 1,
+        date: '01/15/21'
+      },
+      {
+        id: 2,
+        date: '01/16/21',
+      }
+    ]
   },
   {
     id: 2,
     title: 'Carignan',
+    days: [
+      {
+        id: 3,
+        date: '01/15/21'
+      },
+      {
+        id: 4,
+        date: '01/16/21',
+      }
+    ]
   },
 ];
 
+function BatchesScreen({ navigation }) {
+  return (
+    <View>
+      <BatchList batches={batches} navigation={navigation}></BatchList>
+    </View>
+  );
+}
+
+function DaysScreen({ route: {params: { days }}, navigation }) {
+  return (
+    <View>
+      <DayList days={days}></DayList>
+    </View>
+  );
+}
+
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
-        <BatchList style={styles.batchList} batches={batches}></BatchList>
-        <StatusBar style="auto"></StatusBar>
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Batches" >
+        <Stack.Screen name="Batches" component={BatchesScreen}/>
+        <Stack.Screen name="Days" component={DaysScreen}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
