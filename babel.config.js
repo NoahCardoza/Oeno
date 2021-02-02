d6dcd83d3494e50632f96f23d1de146c106a1342
NoahCardoza <noahcardoza@gmail.com>
2021-02-02 12:49:59 -0800

    alias @ to ./src

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,21 @@
-module.exports = function(api) {
-  api.cache(true);
+
+const path = require('path');
+
+module.exports = function (api) {
+  api && api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+    [
+      'module-resolver',
+      {
+        'root': ["./src/"],
+        // 'extensions': [".js", ".ios.js", ".android.js"],
+        'alias': {
+          '@': './src/',
+        }
+      }
+    ]
+  ]
   };
 };
