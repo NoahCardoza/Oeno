2214f6a35795041858ac66dd746e755dee48613b
NoahCardoza <noahcardoza@gmail.com>
2021-02-03 14:50:33 -0800

    move + button to scenes file

--- src/navigations/index.js
+++ src/navigations/index.js
@@ -1,32 +1,48 @@
 import React from 'react';
-
-
 import { createStackNavigator } from '@react-navigation/stack';
-import BatchsScreen from '@/scenes/batches';
+import { BatchesScreen, BatchesScreenOptions } from '@/scenes/batches';
 import DaysScreen from '@/scenes/days';
 import RecordsScreen from '@/scenes/records';
 import RecordScreen from '@/scenes/record';
+import { Icon } from 'react-native-elements'
 
+// React.useState()
 
 const Stack = createStackNavigator()
+
+const AddNewListItem = ({ onPress }) => ({
+  headerRight: () => (
+      <Icon
+        style={{ margin: 10}}
+        name="add"
+        color="#0A84FF"
+        onPress={onPress}  
+    />
+  ),
+})
 
 const RootStack = () => (
   <Stack.Navigator initialRouteName="Batches">
     <Stack.Screen
       name="Batches"
-      component={BatchsScreen}
+      component={BatchesScreen}
+      options={BatchesScreenOptions}
+
     />
     <Stack.Screen
       name="Days"
       component={DaysScreen}
     />
     <Stack.Screen
-        name="Records"
-        component={RecordsScreen}
+      name="Records"
+      component={RecordsScreen}
+      options={AddNewListItem({
+        onPress: record => console.log(`New record: ${record}`)
+      })}
     />
     <Stack.Screen
-        name="Record"
-        component={RecordScreen}
+      name="Record"
+      component={RecordScreen}
     />
   </Stack.Navigator>
 
--- src/scenes/batches/index.js
+++ src/scenes/batches/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { View } from 'react-native';
-import { ListItem } from 'react-native-elements'
+import { View, Alert } from 'react-native';
+import { ListItem, Icon } from 'react-native-elements'
 import { useAsync } from "react-async"
 import { getAllBatches } from '@/database';
 
+let Batches;
+
 const BatchList = ({ batches, navigation }) => batches.map(({ BatchId, BatchName }) => (
   <ListItem
-    key={id}
+    key={BatchId}
     bottomDivider
     onPress={() => navigation.push('Days', { batchId: BatchId })}
   >
@@ -17,15 +19,46 @@ const BatchList = ({ batches, navigation }) => batches.map(({ BatchId, BatchName
   </ListItem>
 ));
 
-function BatchesScreen({ navigation }) {
-  const { data, error } = useAsync({ promiseFn: getAllBatches })
-  if (error) return console.error(error)
+const PromptUser = ({ prompt, onSuccess }) => Alert.prompt(
+      prompt,
+      null,
+      [
+        {
+          text: "Cancel",
+          style: "cancel"
+        },
+        {
+          text: "Save",
+          onPress: onSuccess
+        }
+      ],
+      "plain-text"
+    )
 
+export const BatchesScreenOptions = {
+  headerRight: () => (
+      <Icon
+        style={{ margin: 10}}
+        name="add"
+        color="#0A84FF"
+        onPress={() => PromptUser({
+          prompt: "Enter a new batch name:",
+          onSuccess: BatchName => Batches.setData([{ BatchId: 10, BatchName }, ...Batches.data])
+        })}  
+    />
+  ),
+}
+
+export function BatchesScreen({ navigation }) {
+  Batches = useAsync({ promiseFn: getAllBatches })
+  if (Batches.error) return console.error(Batches.error)
   return (
     <View>
-      <BatchList batches={data || []} navigation={navigation}></BatchList>
+      <BatchList batches={Batches.data || []} navigation={navigation}></BatchList>
     </View>
   );
 }
+
+
 
 export default BatchesScreen;
