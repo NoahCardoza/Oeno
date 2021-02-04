791c81fb3f1fcc8180f9ea532286c9e5e339ea43
NoahCardoza <noahcardoza@gmail.com>
2021-02-03 16:50:18 -0800

    start work on new record scene

---
+++ src/scenes/new-record/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { View } from 'react-native';
+import { ListItem } from 'react-native-elements'
+
+import { getRecordsFromDayId } from '@/database';
+
+import PlusButton from '@/components/PlusButton';
+
+
+const RecordList = ({records, navigation}) => records.map((record) => (
+  <ListItem
+    key={record.id}
+    bottomDivider
+    onPress={() => navigation.push('Record', { record })}
+  >
+        <ListItem.Content>
+          <ListItem.Title>{record.timestamp}</ListItem.Title>
+        </ListItem.Content>
+        <ListItem.Chevron />
+      </ListItem>
+))
+    
+// export const NewRecordScreenOptions = ({ route, navigation}) => ({
+//   headerRight: () => (
+//     <PlusButton onPress={
+//       () => navigation.push('New Record', {
+//         dayId: route.params.dayId
+//       })
+//     }/>
+//   )
+// })
+
+
+export default function NewRecordScreen({ route, navigation }) {
+  const records = getRecordsFromDayId(route.params.dayId)
+  return (
+    <View>
+      <RecordList records={records} navigation={navigation}></RecordList>
+    </View>
+  );
+}
