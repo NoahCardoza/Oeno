ed984f0524ca5b86b752c3e3573ab14e0f2bfa7b
NoahCardoza <noahcardoza@gmail.com>
2021-02-03 16:49:47 -0800

    component for plus button

---
+++ src/components/PlusButton.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Icon } from 'react-native-elements'
+
+const PlusButton = ({ onPress }) => (
+  <Icon
+    style={{ margin: 10}}
+    name="add"
+    color="#0A84FF"
+    onPress={onPress}  
+  />
+)
+
+export default PlusButton
