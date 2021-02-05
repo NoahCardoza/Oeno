075b0b32bb8dc879b7135746ae773010c6990bec
NoahCardoza <noahcardoza@gmail.com>
2021-02-05 03:01:11 -0800

    dynamically render days based on start date of batch

--- src/scenes/days/index.js
+++ src/scenes/days/index.js
@@ -2,30 +2,49 @@ import React from 'react';
 import { View } from 'react-native';
 import { ListItem } from 'react-native-elements'
 
-import { getDaysFromBatchId } from '@/database';
+import { connect } from 'react-redux';
+
+const mapStateToProps = ({ app: { batches } }, props) => ({ batch: batches.find(({BatchId}) => BatchId === props.route.params.BatchId) });
 
 
-const DayList = ({days, navigation}) => days.map(({id, date }) => (
-  <ListItem
-    key={id}
-    bottomDivider
-    onPress={() => navigation.push('Records', { dayId: id })}
-  >
-        <ListItem.Content>
-          <ListItem.Title>{date}</ListItem.Title>
-        </ListItem.Content>
-        <ListItem.Chevron />
-      </ListItem>
-    ))
+const addDays = function(date, days) {
+    const newDate = new Date(date.valueOf());
+    newDate.setDate(newDate.getDate() + days);
+    return newDate;
+}
+
+function getDates(startDate, stopDate) {
+    const dates = [];
+    let currentDate = startDate;
+    while (currentDate <= stopDate) {
+        dates.push(new Date (currentDate));
+        currentDate = addDays(currentDate, 1);
+    }
+    return dates;
+}
 
 
-function DaysScreen({ route,  navigation }) {
-  const days = getDaysFromBatchId(route.params.batchId)
+function DaysScreenComponent({ batch, navigation }) {
+  const startDate = new Date(batch.BatchStartDate)
+  startDate.setHours(0, 0, 0, 0)
+
+  const daysSinceBatchStart = getDates(startDate, new Date()).reverse()
   return (
     <View>
-      <DayList days={days} navigation={navigation}></DayList>
+      {daysSinceBatchStart.map((date) => (
+        <ListItem
+          key={date.getDay()}
+          bottomDivider
+          onPress={() => navigation.push('Records', { BatchId: batch.BatchId, date: date.getTime() })}
+        >
+          <ListItem.Content>
+            <ListItem.Title>{date.toDateString()}</ListItem.Title>
+          </ListItem.Content>
+          <ListItem.Chevron />
+        </ListItem>
+      ))}
     </View>
   );
 }
 
-export default DaysScreen;
+export default connect(mapStateToProps)(DaysScreenComponent);
