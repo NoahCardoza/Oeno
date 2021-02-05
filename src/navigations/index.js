c89396759c70915498d1e0a02de2e3709048d49c
NoahCardoza <noahcardoza@gmail.com>
2021-02-05 03:00:26 -0800

    wrap app with root store

--- src/navigations/index.js
+++ src/navigations/index.js
@@ -5,36 +5,41 @@ import DaysScreen from '@/scenes/days';
 import {RecordsScreen, RecordsScreenOptions } from '@/scenes/records';
 import RecordScreen from '@/scenes/record';
 import NewRecordScreen from '@/scenes/new-record';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootStore from '@/store'
 
+const store = createStore(rootStore);
 const Stack = createStackNavigator()
 
 const RootStack = () => (
-  <Stack.Navigator initialRouteName="Batches">
-    <Stack.Screen
-      name="Batches"
-      component={BatchesScreen}
-      options={BatchesScreenOptions}
+  <Provider store={store}>
+    <Stack.Navigator initialRouteName="Batches">
+      <Stack.Screen
+        name="Batches"
+        component={BatchesScreen}
+        options={BatchesScreenOptions}
 
-    />
-    <Stack.Screen
-      name="Days"
-      component={DaysScreen}
-    />
-    <Stack.Screen
-      name="Records"
-      component={RecordsScreen}
-      options={RecordsScreenOptions}
-    />
-    <Stack.Screen
-      name="Record"
-      component={RecordScreen}
-    />
-    <Stack.Screen
-      name="New Record"
-      component={NewRecordScreen}
-    />
-  </Stack.Navigator>
-
+      />
+      <Stack.Screen
+        name="Days"
+        component={DaysScreen}
+      />
+      <Stack.Screen
+        name="Records"
+        component={RecordsScreen}
+        options={RecordsScreenOptions}
+      />
+      <Stack.Screen
+        name="Record"
+        component={RecordScreen}
+      />
+      <Stack.Screen
+        name="New Record"
+        component={NewRecordScreen}
+      />
+    </Stack.Navigator>
+  </Provider>
 )
 
 export default RootStack;
