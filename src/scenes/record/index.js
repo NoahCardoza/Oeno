31ef65ecccc45cac703cd3754a0140266a3c15a9
NoahCardoza <noahcardoza@gmail.com>
2021-02-05 11:12:50 -0800

    move record input ui list to new component

--- src/scenes/record/index.js
+++ src/scenes/record/index.js
@@ -1,47 +1,18 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
-
-import { getInputsWithTagFromRecordId } from '@/database';
-
-import { Card, ListItem, Badge } from 'react-native-elements'
+import { Card } from 'react-native-elements'
+import RecordInputList from '@/components/RecordInputList'
 
 
-
-
-const RecordList = ({records, navigation}) => records.map((record) => (
-  <ListItem
-    key={record.id}
-    bottomDivider
-    onPress={() => navigation.push('Record', { record })}
-  >
-        <ListItem.Content>
-          <ListItem.Title>{record.timestamp}</ListItem.Title>
-        </ListItem.Content>
-        <ListItem.Chevron />
-      </ListItem>
-    ))
-
-
-function DaysScreen({ route, navigation }) {
+function RecordScreen({ route, navigation }) {
   const { record } = route.params;
   return (
     <View>
       <Card containerStyle={{marginBottom: 20}}>
         <Text>{record.RecordObservation}</Text>
       </Card>
-        {
-          record.RecordInputs.map(({ InputValue, InputTag }, i) => {
-            return (
-              <ListItem key={i} bottomDivider topDivider={i === 0}>
-                <ListItem.Content>
-                  <ListItem.Title>{InputValue}</ListItem.Title>
-                </ListItem.Content>
-                <Badge value={InputTag} textStyle={{ fontWeight: 'bold', fontSize: 15 }} badgeStyle={{ padding: 3, height: null }}/>
-              </ListItem>
-            );
-          })
-        }
+      {RecordInputList(record.RecordInputs)}
     </View>
   );
 }
@@ -50,4 +21,4 @@ const mapStateToProps = ({batches}) => ({ batches })
 
 
 
-export default DaysScreen //connect()(DaysScreen);
+export default RecordScreen //connect()(RecordScreen);
