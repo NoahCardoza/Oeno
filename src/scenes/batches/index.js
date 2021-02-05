2ff435dcd3af18de7f204d02ce37f093d962932a
NoahCardoza <noahcardoza@gmail.com>
2021-02-05 03:00:43 -0800

    load batches from store

--- src/scenes/batches/index.js
+++ src/scenes/batches/index.js
@@ -1,47 +1,60 @@
 import React from 'react';
 import { View } from 'react-native';
 import { ListItem } from 'react-native-elements'
-import { useAsync } from "react-async"
-import { getAllBatches } from '@/database';
+
 import PlusButton from '@/components/PlusButton';
 import Prompt from '@/utils/prompt';
 
-let Batches;
+import { addBatch } from '@/store';
 
-const BatchList = ({ batches, navigation }) => batches.map(({ BatchId, BatchName }) => (
-  <ListItem
-    key={BatchId}
-    bottomDivider
-    onPress={() => navigation.push('Days', { batchId: BatchId })}
-  >
-    <ListItem.Content>
-      <ListItem.Title>{BatchName}</ListItem.Title>
-    </ListItem.Content>
-    <ListItem.Chevron />
-  </ListItem>
-));
+import { connect, } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
-export const BatchesScreenOptions = {
-  headerRight: () => (
+
+const mapStateToProps = ({ app: { batches } }) => ({ batches });
+const mapDispatchToProps = dispatch => bindActionCreators({ addBatch }, dispatch);
+const ReduxConnector = connect(mapStateToProps, mapDispatchToProps);
+
+const NewBatchButton = ReduxConnector(({ addBatch }) => (
     <PlusButton onPress={
       () => Prompt({
           prompt: "Enter a new batch name:",
-          onSuccess: BatchName => Batches.setData([{ BatchId: 10, BatchName }, ...Batches.data])
+          onSuccess: name => addBatch(name)
       })
     }/>
-  ),
+  ))
+
+export const BatchesScreenOptions = {
+  headerRight: () => <NewBatchButton/>
 }
 
-export function BatchesScreen({ navigation }) {
-  Batches = useAsync({ promiseFn: getAllBatches })
-  if (Batches.error) return console.error(Batches.error)
+export function BatchesScreenComponent({ batches, navigation}) {
   return (
     <View>
-      <BatchList batches={Batches.data || []} navigation={navigation}></BatchList>
+      {batches.map(({ BatchId, BatchName }) => (
+      <ListItem
+        key={BatchId}
+        bottomDivider
+        onPress={() => navigation.push('Days', { BatchId })}
+      >
+        
+        <ListItem.Content>
+          <ListItem.Title>{BatchName}</ListItem.Title>
+        </ListItem.Content>
+        <ListItem.Chevron />
+      </ListItem>
+    ))}
     </View>
   );
 }
 
 
 
-export default BatchesScreen;
+
+
+
+export const BatchesScreen = ReduxConnector(BatchesScreenComponent);
+
+
+
+// export default BatchesScreen;
